9f9059c8de397ca07f1621cbeb05d5cc520425ee
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-01-19 22:05:12 +0000

    tidying up code

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,23 +10,36 @@ function flipCard() {
         //first click
         flippedCard = true;
         firstCard = this;
-    } else {
-        //Second click
-        flippedCard = false;
-        secondCard = this;
 
-        if(firstCard.dataset.image === secondCard.dataset.image) {
-            //Match
-            firstCard.removeEventListener("click", flipCard);
-            secondCard.removeEventListener("click", flipCard);
-        } else {
-            //Not a match
-            setTimeout(() => {
-                firstCard.classList.remove("flip");
-                secondCard.classList.remove("flip"); 
-            }, 700);
-        }
+        return;
+
     }
+    //Second click
+    flippedCard = false;
+    secondCard = this;
+
+    checkMatch();
+}
+
+function checkMatch(){
+    //Check to see if cards match
+    let isMatch =  firstCard.dataset.image === secondCard.dataset.image;
+
+    isMatch ? matchedCard() : flipCardBack();
+}
+
+function matchedCard() {
+//Match
+firstCard.removeEventListener("click", flipCard);
+secondCard.removeEventListener("click", flipCard);
+}
+
+function flipCardBack(){
+    //Not a match
+    setTimeout(() => {
+        firstCard.classList.remove("flip");
+        secondCard.classList.remove("flip"); 
+    }, 700);
 }
 
 cards.forEach(square => square.addEventListener('click', flipCard));

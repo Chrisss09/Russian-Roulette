c0ce81af957143b99c6a88aa8b07218a094e6e71
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-01-19 13:54:53 +0000

    added Boolean rules when card is flipped+ testing them with console log

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,17 @@
 const cards = document.querySelectorAll('.card');
 
+let flippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+}
+
+if(!flippedCard) {
+    flippedCard = true;
+    firstCard = this;
+
+    console.log({flippedCard, firstCard});
 }
 
 cards.forEach(square => square.addEventListener('click', flipCard));

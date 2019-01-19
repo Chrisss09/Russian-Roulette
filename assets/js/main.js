945fca30d657b5853215f975b0e4777e55c65df7
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-01-19 15:26:55 +0000

    added comments to JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,16 +7,20 @@ function flipCard() {
     this.classList.add('flip');
 
     if(!flippedCard) {
+        //first click
         flippedCard = true;
         firstCard = this;
     } else {
+        //Second click
         flippedCard = false;
         secondCard = this;
 
         if(firstCard.dataset.image === secondCard.dataset.image) {
+            //Match
             firstCard.removeEventListener("click", flipCard);
             secondCard.removeEventListener("click", flipCard);
         } else {
+            //Not a match
             setTimeout(() => {
                 firstCard.classList.remove("flip");
                 secondCard.classList.remove("flip"); 

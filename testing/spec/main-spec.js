b69aaf80a6517657e17fd99667601c59bc79160d
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-02-15 19:45:27 +0000

    Starting to test my js

---
+++ testing/spec/main-spec.js
@@ -0,0 +1,5 @@
+describe("Russian Roulette", function(){
+    it("should flip card", function(){
+
+    });
+});
--- testing/src/extra.js
+++ testing/src/extra.js
@@ -8,7 +8,7 @@ function resetButton() {
 var loader = document.getElementById("loader");
 
 window.addEventListener("load", function(){
-    loader.style.height = "100%";
-    loader.style.width = "100%";
-    loader.style.visibility = "hidden";
+    // loader.style.height = "100%";
+    // loader.style.width = "100%";
+    //loader.style.visibility = "hidden";
 });

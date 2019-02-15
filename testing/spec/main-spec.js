bb43abde2c76771d0929917f232effdcfc964fe8
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-02-15 20:54:34 +0000

    Added comments to my tests

--- testing/spec/main-spec.js
+++ testing/spec/main-spec.js
@@ -1,8 +1,8 @@
 describe("Russian Roulette", function(){
     it("should check first flip", function(){
-        expect(!flippedCard).toBe(true);
+        expect(!flippedCard).toBe(true); //flippedCard is set to false so this will show as a pass due to '!' operator
     });
     it("should check second flip", function(){
-        expect(flippedCard).toBe(false);
+        expect(flippedCard).toBe(false); //flippedCard is set to false so this will pass
     });
 });

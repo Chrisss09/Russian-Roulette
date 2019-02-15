1fcf172845c277b1e0eebda028b4aaf1498f8723
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-02-15 20:24:57 +0000

    Amended spec of first test to be more suitable

--- testing/spec/main-spec.js
+++ testing/spec/main-spec.js
@@ -1,5 +1,5 @@
 describe("Russian Roulette", function(){
-    it("should check card", function(){
+    it("should check first flip", function(){
         expect(flippedCard).toBe(false);
     });
 });

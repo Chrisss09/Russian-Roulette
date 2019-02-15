1795ab0a50f4539c5a1e174395116777786f6e38
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-02-15 20:41:12 +0000

    Testing second flip

--- testing/spec/main-spec.js
+++ testing/spec/main-spec.js
@@ -1,5 +1,8 @@
 describe("Russian Roulette", function(){
     it("should check first flip", function(){
+        expect(!flippedCard).toBe(true);
+    });
+    it("should check second flip", function(){
         expect(flippedCard).toBe(false);
     });
 });

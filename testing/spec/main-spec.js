b109bdaf993e534d74d553c56dc39f6bca8f853e
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-02-15 22:37:35 +0000

    Tested the flipCardBack function to be correct

--- testing/spec/main-spec.js
+++ testing/spec/main-spec.js
@@ -1,8 +1,20 @@
 describe("Russian Roulette", function(){
     it("should check first flip", function(){
-        expect(!flippedCard).toBe(true); //flippedCard is set to false so this will show as a pass due to '!' operator
+        expect(!flippedCard).toBe(true); 
+        //flippedCard is set to false so this will show as a pass due to '!' operator
     });
     it("should check second flip", function(){
-        expect(flippedCard).toBe(false); //flippedCard is set to false so this will pass
+        expect(flippedCard).not.toBe(true);
+        //flippedCard is set to false so this will pass
+    });
+    it("should flip card back", function(){
+        expect(flipCardBack()).not.toBe(true);
+        //Function flips cards back if false
+    });
+});
+
+xdescribe("flip check", function(){ //temp disabled suit
+    it("should exist", function(){
+        expect(flipCard().not.toBeNull());
     });
 });

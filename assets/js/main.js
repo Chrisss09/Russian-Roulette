a8684f48b6790df12753ed61233d82b2d02b8d99
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-02-24 12:13:21 +0000

    Added a function for user to click outside area to exit modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -105,10 +105,18 @@ btnModal.addEventListener("click", openModal);
 
 btnClose.addEventListener("click", closeModal);
 
+window.addEventListener("click", outsideClick);
+
 function openModal(){
     modal.style.display = "block";
 }
 
 function closeModal(){
     modal.style.display = "none";
+}
+
+function outsideClick(e){
+    if(e.target == modal) {
+    modal.style.display = "none";
+    }
 }
